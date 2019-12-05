c38349d8baf765a05ea82c8cdb4db553ba0e9a07
Jason Benedict <jasonjb82@gmail.com>
2019-12-06 06:53:51 +0800

    Update clean_industrial_dir.R
    cleaning up 2015 directory

--- clean_industrial_dir.R
+++ clean_industrial_dir.R
@@ -19,13 +19,15 @@ dropbox_dir <- file_content$path
 wdir <- paste0(dropbox_dir,"\\kraus\\data\\direktori_industri\\")
 setwd(wdir)
 
+# 2003
+
 # Read pdf file ----------------------------------------------------------
 
 pdf_file = paste0(wdir,"pdf\\Direktori_Industri_Pengolahan_Indonesia_2003-pages-43-59.pdf")
 
 # Cleaning up data -------------------------------------------------------
 
-data <- pdf_data(pdf_file) %>% 
+data_2003 <- pdf_data(pdf_file) %>% 
   bind_rows() %>%
   filter(height <=8) %>%
   # Setting delimiters to get different information in pdf file
@@ -59,6 +61,7 @@ data <- pdf_data(pdf_file) %>%
   mutate(category = ifelse(text =="M", "address",category)) %>% # M is part of address
   mutate(category = ifelse(text =="S", "address",category)) %>% # S is part of address
   mutate(category = ifelse(text =="BUAH DHT", "company_name",category)) %>% # BUAH DHT is part of company name
+  mutate(category = ifelse(text =="SIRINGO RINGO", "company_name",category)) %>% # SIRINGO RINFO is part of company name
   mutate(category = ifelse(lag(category) == "address" & lead(category) == "address","address",category)) %>% # if before and after value in row is address, change in between value to address
   mutate(category = ifelse(lag(category) == "company_name" & lead(category) == "company_name","company_name",category)) %>% # if before and after value in row is company name, change in between value to company name
   mutate(category = ifelse(lag(category) == "department_occupation" & lead(category) == "department_occupation","department_occupation",category)) %>% # if before and after value in row is department occupation, change in between value to department occupation
@@ -78,10 +81,83 @@ data <- pdf_data(pdf_file) %>%
   mutate(no_workers = sub("\\s+[^ ]+$", "",no_workers)) %>% # remove additional numbers in no of workers column
   mutate_all(na_if,"") %>% # remove any rows with no values
   drop_na(main_product) %>% # drop rows with NA's in main product column
-  filter(str_detect(main_product, 'CPO|SAWIT')) # filter only palm products (includes CPO,sawit)
+  filter(str_detect(main_product, 'CPO|SAWIT|PALM')) # filter only palm products (includes CPO,sawit)
 
 
 # Export to excel file --------------------------------------------
 
 output_filename <- tools::file_path_sans_ext(basename(pdf_file))
-write.xlsx(data,file=paste0(wdir,"\\extracted_data\\",output_filename,".xlsx")) 
+write.xlsx(data_2003,file=paste0(wdir,"\\extracted_data\\",output_filename,".xlsx")) 
+
+
+# 2015
+
+# Read pdf file ----------------------------------------------------------
+
+pdf_file = paste0(wdir,"pdf\\48660-ID-direktori-industri-manufaktur-indonesia-2015-pages-63-76.pdf")
+
+# Cleaning up data -------------------------------------------------------
+
+data_2015 <- pdf_data(pdf_file) %>% 
+  bind_rows() %>%
+  filter(height <=8) %>%
+  # Setting delimiters to get different information in pdf file
+  mutate(category = ifelse(text =="^","main_product",
+                           ifelse(text ==";","no_workers",
+                                  ifelse(text =="`","address",
+                                         ifelse(text == "%","tel_no",
+                                                ifelse(text == "#","fax_no",
+                                                       ifelse(text == ":","head_off_address",
+                                                       ifelse(text == "E","email",NA)))))))) %>%
+  fill(category) %>%
+  filter(y != 74) %>% # remove categories
+  mutate(text = str_replace(text,","," ")) %>% # remove commas
+  mutate(text = str_replace(text,"-"," ")) %>% # remove hyphens
+  mutate(text = str_replace(text,'"',"")) %>% # remove quotes
+  mutate(text = str_replace(text,"[.]","")) %>% # remove periods
+  mutate(text = str_replace(text,"/"," ")) %>% # remove forward slashes
+  mutate(text = str_replace(text,"[(]"," ")) %>% # remove left parenthesis
+  mutate(text = str_replace(text,"[)]"," ")) %>% # remove right parenthesis
+  mutate(text = trimws(text,which="right")) %>% # trim right whitespace
+  mutate(text = trimws(text,which="left")) %>% # trim left whitespace
+  mutate(category = ifelse(str_detect(text,"^[:upper:]+$") & category != "main_product","company_name",category)) %>% # anything with uppercase to company name
+  mutate(category = ifelse(str_detect(text,"^[:upper:]+$ ^[:upper:]+$") & category !="company name","company_name",category)) %>% # anything with two uppercase words to company name
+  mutate(category = ifelse(str_detect(text,"PT"),"company_name",category)) %>% # anything with 'PT' to company name
+  mutate(category = ifelse(is.na(category), "company_name",category)) %>% # NA categories at top rows to company name (first company)
+  mutate(category = ifelse(text =="SH", "contact_person",category)) %>% # SH is part of contact person
+  mutate(category = ifelse(text =="SE", "contact_person",category)) %>% # SE is part of contact person
+  #mutate(category = ifelse(text =="E", "email",category)) %>% # E is part of email
+  #mutate(category = ifelse(text =="M", "address",category)) %>% # M is part of address
+  #mutate(category = ifelse(text =="S", "address",category)) %>% # S is part of address
+  mutate(category = ifelse(text =="BUAH DHT", "company_name",category)) %>% # BUAH DHT is part of company name
+  mutate(category = ifelse(text =="SIRINGO RINGO", "company_name",category)) %>% # BUAH DHT is part of company name
+  mutate(category = ifelse(lag(category) == "address" & lead(category) == "address","address",category)) %>% # if before and after value in row is address, change in between value to address
+  mutate(category = ifelse(lag(category) == "company_name" & lead(category) == "company_name","company_name",category)) %>% # if before and after value in row is company name, change in between value to company name
+  #mutate(category = ifelse(lag(category) == "department_occupation" & lead(category) == "department_occupation","department_occupation",category)) %>% # if before and after value in row is department occupation, change in between value to department occupation
+  mutate(category = ifelse(is.na(category),"company_name",category)) %>% # any rows with NA is company name  
+  filter(text != ">" & text != "^" & text != ";" & text != "<" & text != ">" & text != "#" & text != "@" & text != "`" & text != "E" & text != "%" & text != "$" & text != ":") %>% # remove delimiters
+  mutate(category = ifelse(str_detect(text,"@"),"email",category)) %>% # remove emails that are part of company names
+  group_by(category, grp = cumsum(category == "company_name" & lag(category, default = "") != "company_name")) %>%  # split into columns at each company name
+  summarise(desc = paste(text, collapse =  " ")) %>% 
+  pivot_wider(id_cols = grp, names_from = category, values_from = desc) %>%
+  select("company_name","main_product","address","no_workers","tel_no","fax_no","head_off_address","email") %>% #select required columns
+  mutate(main_product = str_replace(main_product,"produksi utama main product","")) %>% # remove erroneous values in column
+  mutate(main_product = str_replace(main_product,"C P O","CPO")) %>% # standardize CPO naming in product column
+  mutate(main_product = str_replace(main_product,"CP.O","CPO")) %>% # standardize CPO naming in product column
+  mutate(no_workers = str_replace(no_workers,"tenaga kerja person engaged","")) %>% # remove erroneous values in column
+  #mutate(head_off_tel_no = str_replace(head_off_tel_no,"telpon kantor pusat head office phone number","")) %>% # remove erroneous values in column
+  mutate(tel_no = str_replace(tel_no,"phone number","")) %>% # remove erroneous values in column
+  mutate(tel_no = str_replace(tel_no,"telepon","")) %>% # remove erroneous values in column
+  mutate(fax_no = str_replace(fax_no,"fax pabrik fax number of establishment","")) %>% # remove erroneous values in column
+  mutate(head_off_address = str_replace(head_off_address,"alamat kantor pusat address of head office telpon kantor pusat head office phone number fax kantor pusat head office fax number",""))	%>%
+  mutate(no_workers = sub("\\s+[^ ]+$", "",no_workers)) %>% # remove additional numbers in no of workers column
+  mutate_all(na_if,"") %>% # remove any rows with no values
+  drop_na(main_product) %>% # drop rows with NA's in main product column
+  filter(str_detect(main_product, 'CPO|SAWIT|PALM|RBDPO')) # filter only palm products (includes CPO,sawit)
+
+
+# Export to excel file --------------------------------------------
+
+output_filename <- tools::file_path_sans_ext(basename(pdf_file))
+write.xlsx(data_2015,file=paste0(wdir,"\\extracted_data\\",output_filename,".xlsx")) 
+
